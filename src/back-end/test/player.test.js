cd8a5df3dc7d99ba2f988927e4e218503453d30b
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-07-08 23:14:52 +0530

    player.test.js creation and creation of test cases

---
+++ src/back-end/test/player.test.js
@@ -0,0 +1,16 @@
+import { Player } from "../player";
+
+const player = new Player("Player", false);
+const computer = new Player("Computer", true);
+
+test("Player attacks computer at (0, 0) : Success", () => {
+  expect(player.playerAttack(0, 0, computer)).toBeTruthy();
+});
+
+test("Player attacks computer at (0, 0) : Already attacked", () => {
+  expect(player.playerAttack(0, 0, computer)).toBeFalsy();
+});
+
+test("Computer attacks player", () => {
+  expect(computer.computerAttack(player)).toBeTruthy();
+});
