6c342781d509f3420b723ef7deca79cde1315702
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-07-09 21:33:23 +0530

    name popup created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./style.css";
+
+const nameWrapper = document.querySelector(".name-textbox-popup-wrapper");
+const dialogModal = document.querySelector(".dialog-modal");
+
+function createNameTextboxPopup() {
+  const enterNameText = document.createElement("p");
+  enterNameText.innerHTML = "Enter your name";
+  enterNameText.classList.add("enter-name-text");
+
+  const nameTextbox = document.createElement("input");
+  nameTextbox.setAttribute("type", "text");
+  nameTextbox.setAttribute("placeholder", "Enter your name");
+  nameTextbox.classList.add("name-textbox");
+
+  const submitButton = document.createElement("button");
+  submitButton.innerHTML = "Submit";
+  submitButton.classList.add("submit-button");
+
+  dialogModal.appendChild(enterNameText);
+  dialogModal.appendChild(nameTextbox);
+  dialogModal.appendChild(submitButton);
+
+  nameWrapper.appendChild(dialogModal);
+}
