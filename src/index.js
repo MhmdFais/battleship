b4051ea0fb6b79920fefcb545e87dded1d0b23a1
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-07-10 22:00:35 +0530

    Project restructuring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./style.css";
 
 const nameTextBox = document.querySelector("#name");
+const nameBtn = document.querySelector("#name-btn");
 
 function getNameFromUser() {
-  return nameTextBox.value;
+  const name = nameTextBox.value;
+
+  nameBtn.addEventListener("click", () => {
+    console.log(name);
+  });
 }
 
 getNameFromUser();
