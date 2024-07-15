13f6d964f9f27fc07268e29d1c1007987bab6539
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-07-15 22:54:31 +0530

    complete redesign

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./style.css";
-import { createPlayerBoard } from "./front-end/playersBoard";
 
 document.addEventListener("DOMContentLoaded", () => {
   const nameContainer = document.querySelector(".name-container");
@@ -8,33 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const humanBoardContainer = document.querySelector(".human-board-container");
   const boardArrangeText = document.querySelector(".user-board-arrange-text");
-
-  const shipOrientationDiv = document.querySelector(".ship-orientation");
-  const orientationBtn = document.querySelector(".orie-btn");
-  const humanBoardDiv = document.querySelector(".human-board");
-  const startGameText = document.querySelector(".start-game");
+  const buttonsContainer = document.querySelector(".buttons-container");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const name = nameTextBox.value;
-    console.log(name);
     nameTextBox.value = "";
 
     nameContainer.style.display = "none";
-
     enablePlayerBoardDisplay(name);
   });
 
   function enablePlayerBoardDisplay(name) {
     enableDisplay();
     showMessage(name);
-    // createPlayerBoard(
-    //   shipOrientationDiv,
-    //   orientationBtn,
-    //   humanBoardDiv,
-    //   startGameText,
-    //   name
-    // );
   }
 
   function showMessage(name) {
@@ -43,5 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function enableDisplay() {
     humanBoardContainer.style.display = "block";
+    buttonsContainer.style.display = "block";
   }
 });
